6f1743113f425ff6cc5ebfc5f233665cc24f478f
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2011-08-16 00:04:09 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdwins::Application.config.secret_token = '8ab1e7366f7b293ebd75819a1963ba02cef5665c02bce9ac566c78338aeb5b9df2d3a0d27af9f118261c8bb84dd7ef17c66e98b60b103733bccd0877c4fb3eef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nerdwins::Application.config.session_store :cookie_store, :key => '_nerdwins_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nerdwins::Application.config.session_store :active_record_store
