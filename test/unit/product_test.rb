de5f64f27d9ff0fd8cf9bb03fa522e1d80f2f35c
Miguel Akira <miguel.akira@gmail.com>
2011-09-06 01:46:26 -0300

    adicionados testes diversos (unit e functional)

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -14,11 +15,7 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new( 
-        :title          => "My Book title",
-        :description    => "yyy",
-        :image_url      => "zzz.jpg",
-        :ref            => "0001" )
+    product = products(:valid_tshirt)
     product.price = -1
     assert product.invalid?
     assert_equal "must be greater than or equal to 0.01",
@@ -33,6 +30,42 @@ class ProductTest < ActiveSupport::TestCase
         assert product.valid?
     end
 
+    def new_product(image_url)
+        Product.new( 
+        :title          => "My Book title",
+        :description    => "yyy",
+        :price          => 1,
+        :ref            => "0001", 
+        :image_url      => image_url )
+    end
+
+    test "image url" do 
+        ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg 
+            http://a.b.c/x/y/z/fred.gif }
+        bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+        ok.each do |name|
+            assert new_product(name).valid?, "#{name} shouldn't be invalid"
+        end
+
+        bad.each do |name|
+            assert new_product(name).invalid?, "#{name} shouldn't be valid"
+        end
+    end
+
+    test "product title must be unique" do
+        product = Product.new( 
+            :title          => products(:valid_tshirt).title,
+            :description    => "yyy",
+            :price          => 1,
+            :ref            => "0001", 
+            :image_url      => "image_url.png" )
+        
+        assert !product.save
+        assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join('; ')
+
+
+    end
 
 
 
