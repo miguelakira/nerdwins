7ef00cf942bd8db173ed3ad813c8e260982e5ee5
Miguel Akira <miguel.akira@gmail.com>
2011-09-11 20:17:39 -0300

    functional tests e store front

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+
 	validates :title, :description, :image_url, :ref, :presence => true
 	validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
 	validates :title, :uniqueness => true, :length => { :minimum => 10, :message => "Título deve ser maior que 10 caracteres" }
