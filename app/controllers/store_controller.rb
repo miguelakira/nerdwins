77b07d35647634dfdaabea7f5a635931937f16cb
Miguel Akira <miguel.akira@gmail.com>
2011-09-21 01:35:51 -0300

    adicionado carrinho de compras

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @time = Time.now
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 # encoding: utf-8
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
 
 	validates :title, :description, :image_url, :ref, :presence => true
 	validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
@@ -10,4 +13,12 @@ class Product < ActiveRecord::Base
 		:message 	=> 'O arquivo precisa ser no format GIF, JPG ou PNG!'
 	}
 
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      erros.add(:base, 'Line items present')
+      return false
+    end
+  end
 end
